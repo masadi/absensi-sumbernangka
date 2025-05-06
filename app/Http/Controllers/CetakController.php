5d749df9b660a5a3616d205f82e74e3b758ff649
masadi <masadi.com@gmail.com>
2025-05-06 12:00:43 +0700

    update ukuran font nama

--- app/Http/Controllers/CetakController.php
+++ app/Http/Controllers/CetakController.php
@@ -206,7 +206,7 @@ class CetakController extends Controller
             'margin_right' => 0,
             'margin_top' => 0,
             'margin_bottom' => 0,
-            'format'        => [54, 85.6],
+            'format'        => [54, 85.1],
         ]);
         $pdf->showImageErrors = true;
         return $pdf->stream(clean($nama).'.pdf');
--- resources/views/cetak/kartu-pd.blade.php
+++ resources/views/cetak/kartu-pd.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="rounded" style="background-image: url({{ asset('storage/' . $item->photo) }})"></div>
     <div class="kotak">
-        <div class="nama {{ strlen($item->nama) > 25 ? 'panjang' : '' }}"><strong>{{ $item->nama }}</strong></div>
+        <div class="nama {{ strlen($item->nama) > 20 ? 'panjang' : '' }}"><strong>{{ $item->nama }}</strong></div>
         <div class="nisn"><strong>{{ $item->tetala }}</strong></div>
         <div class="nisn"><strong>{{ $item->nisn }}</strong></div>
         <div class="kelas"><strong>{{ $item->kelas?->nama_jurusan }}</strong></div>
