baa5ea8f1400c30e8526f9727dc025741f6ee405
masadi <masadi.com@gmail.com>
2025-05-10 11:17:05 +0700

    update

--- resources/views/cetak/kartu-pd-smp.blade.php
+++ resources/views/cetak/kartu-pd-smp.blade.php
@@ -5,7 +5,7 @@
         <div class="nama {{ strlen($item->nama) > 20 ? 'panjang' : '' }}"><strong>{{ $item->nama }}</strong></div>
         <div class="nisn"><strong>{{ $item->tetala }}</strong></div>
         <div class="nisn"><strong>{{ $item->nisn }}</strong></div>
-        <div class="kelas"><strong>{{ $item->kelas?->nama_jurusan }}</strong></div>
+        <!--div class="kelas"><strong>{{ $item->kelas?->nama_jurusan }}</strong></div-->
     </div>
     <div class="qrcode">
         <img src="data:image/svg+xml;base64, {!! $qrcode !!}" width="50">
